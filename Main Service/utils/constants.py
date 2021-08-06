11a8af601b97cea9b50d587aecdff1398d3cde14
NotAlessandroBeatini <82681229+NotAlessandroBeatini@users.noreply.github.com>
2021-08-06 13:21:42 +0200

    Delete Main Service directory

--- Main Service/utils/constants.py
+++
@@ -1,8 +0,0 @@
-port = 587  # For starttls
-smtp_server = "smtp.gmail.com"
-sender_email = "helpFromApes@gmail.com"
-receiver_email = "helpFromApes@gmail.com"
-password = '123456QWERTY!'
-EMAIL = 'helpfromapes@gmail.com'
-PASSWORD = '123456QWERTY!'
-SERVER = 'imap.gmail.com'
